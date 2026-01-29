c4752bc17dbe09faca8864760ce69c29fc18d0ff
barraquad <pauliarto13@gmail.com>
2026-01-29 21:22:34 +0200

    Add files via upload

---
+++ extract.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function extract_7z() {
+
+	# We need to use newline as a field separator
+	IFS_B=${IFS}
+	IFS="
+	"
+
+	output=($(ls -1 *.7z))
+	
+	for i in "${output[@]}"; do
+		7z x "$i" -o* -y
+	
+	done
+	# '@' basically means multiple arguments(an array of elements, not just one)
+	
+	IFS=${IFS_B}
+}
+
+extract_7z
+
